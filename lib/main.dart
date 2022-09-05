a1c527b189040f971f1aabac5caa9af185b7976b
Karthik Sudhan <sudhan260703@gmail.com>
2022-09-05 15:21:09 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'English Word Generator',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Word Generator'),
+        ),
+        body: const Center(
+          child: RandomWords(),
+
+        ),
+      ),
+    );
+  }
+}
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
+  }
+  
+
+}
